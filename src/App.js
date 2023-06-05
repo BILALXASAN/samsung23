e3c2077c4c0095d5530850d87ae2f3f2add01307
BILALXASAN <135513909+BILALXASAN@users.noreply.github.com>
2023-06-05 14:11:57 +0500

    downloading

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className='shadow'>
+        <div className="container-fluid py-4">
+          <img src="" alt="" />
+        </div>
+      </header>
+
+      <section id='products' className='py-3'>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img className='d-block' src="https://m.media-amazon.com/images/I/61j99uUfXNL.jpg" alt="" width={200} />
+                <a href="#" className='groupName d-block mb-2 mt-3'>Mobil qurilma</a>
+                <p className='price mb-2'>13 900 000 so'm</p>
+                <p className='priceMonth'>1 200 000 so'm / oyiga</p>
+                <p className='title'>Samsung Galaxy S23 Ultra 12/256 GB Cream</p>
+                <a href="#" className='btn'>Sotib olish</a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img className='d-block' src="https://m.media-amazon.com/images/I/61j99uUfXNL.jpg" alt="" width={200} />
+                <a href="#" className='groupName d-block mb-2 mt-3'>Mobil qurilma</a>
+                <p className='price mb-2'>13 900 000 so'm</p>
+                <p className='priceMonth'>1 200 000 so'm / oyiga</p>
+                <p className='title'>Samsung Galaxy S23 Ultra 12/256 GB Cream</p>
+                <a href="#" className='btn'>Sotib olish</a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img className='d-block' src="https://m.media-amazon.com/images/I/61j99uUfXNL.jpg" alt="" width={200} />
+                <a href="#" className='groupName d-block mb-2 mt-3'>Mobil qurilma</a>
+                <p className='price mb-2'>13 900 000 so'm</p>
+                <p className='priceMonth'>1 200 000 so'm / oyiga</p>
+                <p className='title'>Samsung Galaxy S23 Ultra 12/256 GB Cream</p>
+                <a href="#" className='btn'>Sotib olish</a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img className='d-block' src="https://m.media-amazon.com/images/I/61j99uUfXNL.jpg" alt="" width={200} />
+                <a href="#" className='groupName d-block mb-2 mt-3'>Mobil qurilma</a>
+                <p className='price mb-2'>13 900 000 so'm</p>
+                <p className='priceMonth'>1 200 000 so'm / oyiga</p>
+                <p className='title'>Samsung Galaxy S23 Ultra 12/256 GB Cream</p>
+                <a href="#" className='btn'>Sotib olish</a>
+              </div>
+            </div>
+            <div className="col-sm-6 col-md-4 col-lg-3 productCol">
+              <div className="productCard p-3">
+                <img className='d-block' src="https://m.media-amazon.com/images/I/61j99uUfXNL.jpg" alt="" width={200} />
+                <a href="#" className='groupName d-block mb-2 mt-3'>Mobil qurilma</a>
+                <p className='price mb-2'>13 900 000 so'm</p>
+                <p className='priceMonth'>1 200 000 so'm / oyiga</p>
+                <p className='title'>Samsung Galaxy S23 Ultra 12/256 GB Cream</p>
+                <a href="#" className='btn'>Sotib olish</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
